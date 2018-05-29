b0c1995139283224944cfe7030073e838391c610
Michael Benfield <mike.benfield@gmail.com>
2018-05-28 17:29:22 -0700

    VDP line interrupt timing fixes.

--- attalus/src/hardware/sms_vdp/line.rs
+++ attalus/src/hardware/sms_vdp/line.rs
@@ -17,7 +17,12 @@ where
         // manifests::SET_FRAME_INTERRUPT.send(vdp, Payload::U16([v, 0, 0, 0]));
     }
 
-    if v <= vdp.active_lines() {
+    let new_v = (v + 1) % vdp.total_lines();
+
+    vdp.set_v(new_v);
+
+    if new_v <= vdp.active_lines() {
+        // yes, according to VDPTEST.sms, this really should be <=
         let line_counter = vdp.line_counter();
         vdp.set_line_counter(line_counter.wrapping_sub(1));
         if vdp.line_counter() == 0xFF {
@@ -32,10 +37,6 @@ where
         let reg_line_counter = vdp.reg_line_counter();
         vdp.set_line_counter(reg_line_counter);
     }
-
-    let new_v = (v + 1) % vdp.total_lines();
-
-    vdp.set_v(new_v);
 
     if new_v == 0 {
         let reg9 = unsafe { vdp.register_unchecked(9) };
